94e0cbb157cce5677366ae80bdec73222ef9e77b
GinaZambrano1 <114261260+GinaZambrano1@users.noreply.github.com>
2022-09-28 18:17:22 -0500

    Update historico.py

--- productApp/models/historico.py
+++ productApp/models/historico.py
@@ -4,7 +4,7 @@ from .registroSignosVitales import SignosVitales
 
 class HistoriaClinica(models.Model):
     id_historia = models.AutoField(primary_key=True)
-    signos_vitales = models.ForeignKey(SignosVitales, related_name='registroSignosVitales', on_delete=models.CASCADE)
+    signos_vitales = models.ForeignKey(SignosVitales, related_name='HistoriaClinica', on_delete=models.CASCADE)
     resumen_historia_clinica = models.CharField('historiasClinicas', max_length = 100)
     medicamentos_terapias = models.CharField('medicamentos_terapias', max_length = 100)
     diagnostico = models.CharField('diagnostico', max_length = 100)
